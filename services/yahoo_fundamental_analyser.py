86b80d88e3bfedd974358f8a5b1fbd6082759a34
dmachnia <01178558@pw.edu.pl>
2025-05-14 15:59:20 +0200

    Add FundamentalAnalyser class for financial analysis using Yahoo Finance

--- services/yahoo_fundamental_analyser.py
+++ services/yahoo_fundamental_analyser.py
@@ -1,10 +1,24 @@
 import yfinance as yf
 
-class FundamentalAnalyzer:
+
+class FundamentalAnalyser:
+    """
+    A class to analyze the financial fundamentals of a company using Yahoo Finance.
+
+    Attributes:
+        stock (yfinance.Ticker): An instance of the yfinance Ticker class for the given stock symbol.
+    """
+
     def __init__(self, ticker: str):
         self.stock = yf.Ticker(ticker)
 
-    def get_financial_summary(self):
+    def get_financial_summary(self) -> dict:
+        """
+        Fetches and returns a summary of key financial metrics for the company.
+
+        Returns:
+            dict: A dictionary containing key financial metrics such as ROE, EBITDA, margins, debt, etc.
+        """
         info = self.stock.info
         return {
             "Company": info.get("shortName"),
