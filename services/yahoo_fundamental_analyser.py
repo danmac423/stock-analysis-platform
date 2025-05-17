ffbed1b57c6525b0324c35e70498900ff9749d4c
dmachnia <01178558@pw.edu.pl>
2025-05-17 10:53:50 +0200

    Add AlphaVantageFundamentalAnalyser and Yahoo analysis classes; remove CompanyNewsFetcher

--- services/yahoo_fundamental_analyser.py
+++ services/yahoo_fundamental_analyser.py
@@ -1,33 +1,14 @@
 import yfinance as yf
 
 
-class FundamentalAnalyser:
-    """
-    A class to analyze the financial fundamentals of a company using Yahoo Finance.
-
-    Attributes:
-        stock (yfinance.Ticker): An instance of the yfinance Ticker class for the given stock symbol.
-    """
-
+class YahooFundamentalAnalyser:
     def __init__(self, ticker: str):
         self.stock = yf.Ticker(ticker)
 
-    def get_financial_summary(self) -> dict:
+    def fetch_fundamentals(self) -> dict:
         """
-        Fetches and returns a summary of key financial metrics for the company.
-
+        Fetches various fundamental data related to the stock ticker.
         Returns:
-            dict: A dictionary containing key financial metrics such as ROE, EBITDA, margins, debt, etc.
+            dict: A dictionary containing different types of fundamental data.
         """
-        info = self.stock.info
-        return {
-            "Company": info.get("shortName"),
-            "ROE": info.get("returnOnEquity"),
-            "EBITDA": info.get("ebitda"),
-            "Gross Margin": info.get("grossMargins"),
-            "Operating Margin": info.get("operatingMargins"),
-            "Debt to Equity": info.get("debtToEquity"),
-            "Current Ratio": info.get("currentRatio"),
-            "Total Debt": info.get("totalDebt"),
-            "Total Revenue": info.get("totalRevenue"),
-        }
+        return self.stock.get_info()
