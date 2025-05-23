1bd28f728ac0aaaf9d87f49a06993b19d7a587a7
dmachnia <01178558@pw.edu.pl>
2025-05-23 23:16:18 +0200

    Initialize stock-analysis-platform project with setup configuration and build system requirements

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="stock-analysis-platform",
+    version="0.1",
+    packages=find_packages(),
+    author="Daniel Machniak & Rafał Kowalczuk"
+)
